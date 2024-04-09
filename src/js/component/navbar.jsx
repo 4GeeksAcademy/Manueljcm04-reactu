0eeaa5caabf3a389fdfe2a511b6f818d0797fdb9
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-04-09 19:46:11 +0000

    react

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,35 +1,31 @@
-import React from "react";
+import React from "react"
 
-const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-black">
-            <div className="container-fluid">
-                <a className="navbar-brand text-white" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-white" href="#">Features</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-white" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled text-white" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-    );
-};
-
-
-
-export default NavBar;
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-black">
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">About</a>
+            </li>
+              <li className="nav-item">
+              <a className="nav-link text-white" href="#">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">Contact</a>              
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+)
+}
+export default Navbar
