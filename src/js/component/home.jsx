9e0555ad1fca414ec863207f269f0cfdbdfe13a2
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-04-16 19:11:20 +0000

    reactu

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,14 +11,15 @@ const Home = () => {
 			<NavBar />
 			<div className="mt-4 container">
 				<Jumbotron />
-				<div className="mt-4 d-flex">
+				<div className="row mt-4 justify-content-between mb-5">
 					<Card title="Card 1" description="Card 1 description" button="Button 1"/>
 					<Card title="Card 2" description="Card 2 description" button="Button 2"/>
 					<Card title="Card 3" description="Card 3 description" button="Button 3"/>
 					<Card title="Card 4" description="Card 4 description" button="Button 4"/>
 				</div>
-				<Footer />
+				
 			</div>
+			<Footer />
 		</div>
 	);
 };
